86323d6744e754d04090d3bc1abe92fc89ab3430
Pablo Olivares <65406121+pab1s@users.noreply.github.com>
2021-03-16 16:57:13 +0100

    Package & Module Practice

---
+++ mymodule.py
@@ -0,0 +1,16 @@
+def generate_full_name(firstname, lastname):
+      space = ' '
+      fullname = firstname + space + lastname
+      return fullname
+
+def sum_two_nums (num1, num2):
+    return num1 + num2
+gravity = 9.81
+person = {
+    "firstname": "Pablo",
+    "age": 20,
+    "country": "Spain",
+    "city":'Granada'
+}
+
+
